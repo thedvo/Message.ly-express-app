6947859879688dcacb5f8b90d04953664ca53b51
Dan Vo <danvo.vo@gmail.com>
2022-05-21 20:07:30 -0700

    completed user, messages, auth routes

--- data.sql
+++ data.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
 
+
 CREATE TABLE users (
     username text PRIMARY KEY,
     password text NOT NULL,
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
+const express = require('express');
+const ExpressError = require('../expressError');
+const User = require('../models/User');
+const { ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth');
+
+const router = new express.Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get('/', ensureLoggedIn, async function (req, res, next) {
+	try {
+		let users = await User.all();
+
+		return res.json({ users });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** GET /:username - get detail of users.
  *
@@ -11,6 +27,16 @@
  *
  **/
 
+router.get('/:username', ensureCorrectUser, async function (req, res, next) {
+	try {
+		let { username } = req.params.username;
+		let user = await User.get(username);
+
+		return res.json({ user });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** GET /:username/to - get messages to user
  *
@@ -22,6 +48,16 @@
  *
  **/
 
+router.get('/:username/to', ensureCorrectUser, async function (req, res, next) {
+	try {
+		const { username } = req.params.username;
+		const messages = await User.messagesTo(username);
+
+		return res.json({ messages });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +68,20 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get(
+	'/:username/from',
+	ensureCorrectUser,
+	async function (req, res, next) {
+		try {
+			const { username } = req.params.username;
+			const messages = await User.messagesFrom(username);
+
+			return res.json({ messages });
+		} catch (e) {
+			return next(e);
+		}
+	}
+);
+
+module.exports = router;
